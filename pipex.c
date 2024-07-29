dc79cedec5ec7e70504d1f26fc7713f11b950565
Daniel Springer <daspring@student.42heilbronn.de>
2024-07-29 14:25:24 +0200

    modified pipex.c - now not working

--- pipex.c
+++ pipex.c
@@ -6,13 +6,16 @@
 /*   By: daspring <daspring@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 14:11:57 by daspring          #+#    #+#             */
-/*   Updated: 2024/07/29 14:12:33 by daspring         ###   ########.fr       */
+/*   Updated: 2024/07/29 14:24:50 by daspring         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <unistd.h>
 
 int	main(void)
 {
+	execve(const char *file, char *const *argv, char *const *envp);
+
 	return (EXIT_SUCCESS);
 }
