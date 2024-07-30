a117236ca3d40cfcbe5609d4c35a56dfc0de985b
Daniel Springer <daspring@student.42heilbronn.de>
2024-07-30 14:20:46 +0200

    pipex can execute external commands

--- pipex.c
+++ pipex.c
@@ -6,16 +6,16 @@
 /*   By: daspring <daspring@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 14:11:57 by daspring          #+#    #+#             */
-/*   Updated: 2024/07/29 14:24:50 by daspring         ###   ########.fr       */
+/*   Updated: 2024/07/30 14:17:18 by daspring         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <unistd.h>
 
-int	main(void)
+int	main(int argc, char *argv[], char *envp[])
 {
-	execve(const char *file, char *const *argv, char *const *envp);
+	execve("/Users/daspring/c-programming/my_projects/git_home/pipex/helloWorld", NULL, envp);
 
 	return (EXIT_SUCCESS);
 }
