b55b848dc20db7a37fb8f2f4e47d5b7c307fb999
Daniel Springer <daspring@student.42heilbronn.de>
2024-07-30 18:54:54 +0200

    using argv to specify file names

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: daspring <daspring@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 14:11:57 by daspring          #+#    #+#             */
-/*   Updated: 2024/07/30 18:40:48 by daspring         ###   ########.fr       */
+/*   Updated: 2024/07/30 18:52:59 by daspring         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,25 +29,12 @@ int	main(int argc, char *argv[], char *envp[])
 	pid = fork();
 	if (pid == 0)
 	{
-		// int filedes = open("./input_file", O_RDONLY);
-		dup2(fd[1], STDOUT_FILENO);
-		// dup2(filedes, STDIN_FILENO);
-		// run_first_command(argv, fd, envp);
-
-		execve("./helloWorld", NULL, envp);
+		run_first_command(argv, fd, envp);
 	}
 	else
 	{
-		wait(NULL);
-		int filedes = open("./output_file", O_WRONLY | O_CREAT | O_TRUNC, 0777);
-		dup2(filedes, STDOUT_FILENO);
-		dup2(fd[0], STDIN_FILENO);
-		close(fd[1]);
-		close(filedes);
-		// run_second_command(argv, fd, envp);
-		// execve("./helloWorld", NULL, envp);
-		char	*argv2[] = {"cat", NULL};
-		execv("/bin/cat", argv2);
+		// wait(NULL);
+		run_second_command(argv, fd, envp);
 	}
 	return (EXIT_SUCCESS);
 }
@@ -56,27 +43,28 @@ void	run_second_command(char *argv[], int fd[], char *envp[])
 {
 	int	filedes;
 
-	filedes = open("./output_file", O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	filedes = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
 	if (filedes == -1)
 		exit(EXIT_FAILURE);
 	dup2(filedes, STDOUT_FILENO);
-	// dup2(fd[0], STDIN_FILENO);
-	// close(fd[1]);
-	// close(filedes);
-	// execve("/bin/cat", NULL, envp);
-	execve("./helloWorld", NULL, envp);
+	dup2(fd[0], STDIN_FILENO);
+	close(fd[1]);
+	close(filedes);
+	char	*argv2[] = {"cat", NULL};
+	execve("/bin/cat", argv2, envp);
 }
 
 void	run_first_command(char *argv[], int fd[], char *envp[])
 {
 	int	filedes;
 
-	filedes = open("./input_file", O_RDONLY);
+	filedes = open(argv[1], O_RDONLY, 0777);
 	if (filedes == -1)
 		exit (EXIT_FAILURE);
 	dup2(fd[1], STDOUT_FILENO);
 	dup2(filedes, STDIN_FILENO);
 	close(fd[0]);
 	close(filedes);
-	execve("/bin/cat", NULL, envp);
+	char	*argv2[] = {"cat", NULL};
+	execve("/bin/cat", argv2, envp);
 }
