5244dde770721aa288a4659578ed20c6e5763d6f
Daniel Springer <daspring@student.42heilbronn.de>
2024-07-30 18:41:59 +0200

    somewhat working pipe

--- pipex.c
+++ pipex.c
@@ -6,39 +6,77 @@
 /*   By: daspring <daspring@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 14:11:57 by daspring          #+#    #+#             */
-/*   Updated: 2024/07/30 17:05:19 by daspring         ###   ########.fr       */
+/*   Updated: 2024/07/30 18:40:48 by daspring         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <sys/fcntl.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/wait.h>
 
-void	run_first_command(char *envp[]);
+void	run_second_command(char *argv[], int fd[], char *envp[]);
+void	run_first_command(char *argv[], int fd[], char *envp[]);
 
 int	main(int argc, char *argv[], char *envp[])
 {
-	// int		fd[2];
+	int		fd[2];
 	pid_t	pid;
 
+	if (pipe(fd) == -1)
+		return (EXIT_FAILURE);
 	pid = fork();
 	if (pid == 0)
-		run_first_command(envp);
+	{
+		// int filedes = open("./input_file", O_RDONLY);
+		dup2(fd[1], STDOUT_FILENO);
+		// dup2(filedes, STDIN_FILENO);
+		// run_first_command(argv, fd, envp);
+
+		execve("./helloWorld", NULL, envp);
+	}
 	else
 	{
 		wait(NULL);
-		write(1, "back to parent\n", 15);
+		int filedes = open("./output_file", O_WRONLY | O_CREAT | O_TRUNC, 0777);
+		dup2(filedes, STDOUT_FILENO);
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[1]);
+		close(filedes);
+		// run_second_command(argv, fd, envp);
+		// execve("./helloWorld", NULL, envp);
+		char	*argv2[] = {"cat", NULL};
+		execv("/bin/cat", argv2);
 	}
 	return (EXIT_SUCCESS);
 }
 
-void	run_first_command(char *envp[])
+void	run_second_command(char *argv[], int fd[], char *envp[])
 {
-	int filedes = open("./output_file", O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	int	filedes;
+
+	filedes = open("./output_file", O_WRONLY | O_CREAT | O_TRUNC, 0777);
 	if (filedes == -1)
 		exit(EXIT_FAILURE);
 	dup2(filedes, STDOUT_FILENO);
+	// dup2(fd[0], STDIN_FILENO);
+	// close(fd[1]);
+	// close(filedes);
+	// execve("/bin/cat", NULL, envp);
+	execve("./helloWorld", NULL, envp);
+}
+
+void	run_first_command(char *argv[], int fd[], char *envp[])
+{
+	int	filedes;
+
+	filedes = open("./input_file", O_RDONLY);
+	if (filedes == -1)
+		exit (EXIT_FAILURE);
+	dup2(fd[1], STDOUT_FILENO);
+	dup2(filedes, STDIN_FILENO);
+	close(fd[0]);
 	close(filedes);
-	execve("/Users/daspring/c-programming/my_projects/git_home/pipex/helloWorld", NULL, envp);
+	execve("/bin/cat", NULL, envp);
 }
