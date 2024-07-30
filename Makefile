53e4506b30e83350cbafb0cd1b4d882f43239be6
Daniel Springer <daspring@student.42heilbronn.de>
2024-07-30 17:02:39 +0200

    implemented stdout redirection

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: daspring <daspring@student.42heilbronn.de  +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/07/29 12:20:34 by daspring          #+#    #+#              #
-#    Updated: 2024/07/29 12:25:36 by daspring         ###   ########.fr        #
+#    Updated: 2024/07/30 15:32:19 by daspring         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,12 +17,14 @@ CFLAGS= -wall -werror -wextra -O3
 NAME=pipex
 
 $(NAME) : pipex.c
-	cc pipex.c -o $(NAME)
+	@cc pipex.c -o $(NAME)
+	@echo "created pipex"
 
 all : $(NAME)
 
 fclean :
 	@rm -f $(NAME)
+	@echo "removed pipex"
 
 re : fclean all
 
--- pipex.c
+++ pipex.c
@@ -6,16 +6,38 @@
 /*   By: daspring <daspring@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 14:11:57 by daspring          #+#    #+#             */
-/*   Updated: 2024/07/30 14:17:18 by daspring         ###   ########.fr       */
+/*   Updated: 2024/07/30 15:47:50 by daspring         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
+
+void	run_first_command(char *envp[]);
 
 int	main(int argc, char *argv[], char *envp[])
 {
-	execve("/Users/daspring/c-programming/my_projects/git_home/pipex/helloWorld", NULL, envp);
+	// int		fd[2];
+	pid_t	pid;
 
+	pid = fork();
+	if (pid == 0)
+		run_first_command(envp);
+	else
+	{
+		wait(NULL);
+		write(1, "back to parent\n", 15);
+	}
 	return (EXIT_SUCCESS);
+}
+
+void	run_first_command(char *envp[])
+{
+	int filedes = open("./output_file", O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (filedes == -1)
+		exit(EXIT_FAILURE);
+	dup2(filedes, STDOUT_FILENO);
+	close(filedes);
+	execve("/Users/daspring/c-programming/my_projects/git_home/pipex/helloWorld", NULL, envp);
 }
